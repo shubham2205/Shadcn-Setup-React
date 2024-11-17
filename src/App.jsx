083a1f6bf9c9fbf092a6f13c2388bdd7e0af4d4d
Shubham Dharpure <shubhamdharpure01@gmail.com>
2024-11-17 16:24:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
